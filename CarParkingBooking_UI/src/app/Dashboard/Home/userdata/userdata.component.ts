d5146b8670324bfc2cd599e4ece8c598629d29f7
Infinityhellking <154657173+Infinityhellking@users.noreply.github.com>
2024-09-26 16:21:41 +0530

    user data changes

--- CarParkingBooking_UI/src/app/Dashboard/Home/userdata/userdata.component.ts
+++ CarParkingBooking_UI/src/app/Dashboard/Home/userdata/userdata.component.ts
@@ -9,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class UserdataComponent {
 
+  vechilemodal(){
+    document.getElementById("myDropdown")!.classList.toggle("show");
+  }
+  
+  filterFunction() {
+    const input: HTMLInputElement | null = document.getElementById("myInput") as HTMLInputElement;
+    const filter: string = input.value.toUpperCase();
+    const div: HTMLElement | null = document.getElementById("myDropdown");
+    const a: HTMLAnchorElement[] = Array.from(div!.getElementsByTagName("a"));
+    
+    for (let i: number = 0; i < a.length; i++) {
+      const txtValue: string = a[i].textContent || a[i].innerText || "";
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+
+
+
+
 }
