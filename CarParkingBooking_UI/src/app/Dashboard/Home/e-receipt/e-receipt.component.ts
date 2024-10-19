d300b114295073942f46ddcd928b8d42c65c8eb1
Infinityhellking <154657173+Infinityhellking@users.noreply.github.com>
2024-10-19 12:11:48 +0530

    ui updated

--- CarParkingBooking_UI/src/app/Dashboard/Home/e-receipt/e-receipt.component.ts
+++ CarParkingBooking_UI/src/app/Dashboard/Home/e-receipt/e-receipt.component.ts
@@ -9,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class EReceiptComponent {
 
+
+  printInvoice(){
+    window.print();
+}
+
+const container: HTMLElement | null = document.querySelector(".container");
+const qrInput: HTMLInputElement | null = container?.querySelector(".form input") as HTMLInputElement;
+const generateBtn: HTMLButtonElement | null = container?.querySelector(".form button") as HTMLButtonElement;
+const qrImg: HTMLImageElement | null = container?.querySelector(".qr-code img") as HTMLImageElement;
+
+ preValue: string | undefined;
+
+  generateBtn?.addEventListener("click", () => {
+    const qrValue: string = qrInput?.value.trim() || '';
+    if (!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+
+    qrImg.addEventListener("load", () => {
+        container?.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+
+qrInput?.addEventListener("keyup", () => {
+    if (!qrInput.value.trim()) {
+        container?.classList.remove("active");
+        preValue = "";
+    }
+});
+
+
+
+
+
 }
