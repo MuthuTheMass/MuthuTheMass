5bbc20a1b63108fa58aa25b8b102e9af6713b270
Infinityhellking <154657173+Infinityhellking@users.noreply.github.com>
2025-01-17 21:37:44 +0530

    changes done

---
+++ CarParkingSystem.Domain/Dtos/Booking/BookingDto.cs
@@ -0,0 +1,24 @@
+﻿using CarParkingSystem.Domain.ValueObjects;
+using CarParkingSystem.Infrastructure.Database.CosmosDatabase.Entities;
+
+namespace CarParkingSystem.Application.Dtos.Booking
+{
+    public class BookingDto
+    {
+        public string? DealerId { get; set; }
+
+        public string? CustomerId { get; set; }
+
+        public string? VehicleId { get; set; }
+
+        public required BookingSources BookingSource { get; set; }
+
+        public required CarBookingDates BookingDate { get; set; }
+
+        public string? GeneratedQrCode { get; set; }
+
+        public string? AdvanceAmount { get; set; }
+
+        public required Status BookingStatus { get; set; }
+    }
+}
