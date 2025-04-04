712c236d84e1e8d3e1ec2af73a3c0dd82fb75ec3
shambalaji368 <166840578+shambalaji368@users.noreply.github.com>
2025-04-04 20:22:00 +0530

    merge fix

--- CarParkingBooking.QRCodeGenerator/obj/Debug/net8.0/CarParkingBooking.QRCodeGenerator.AssemblyInfo.cs
+++ CarParkingBooking.QRCodeGenerator/obj/Debug/net8.0/CarParkingBooking.QRCodeGenerator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarParkingBooking.QRCodeGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+043f1b817eb30efcfa14095a92663651a438bf91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae884a859288d58729dadda24721743382af6802")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarParkingBooking.QRCodeGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarParkingBooking.QRCodeGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
