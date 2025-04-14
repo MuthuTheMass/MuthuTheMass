cbe8ff0f46989180c833fb995a195acd2a044f90
shambalaji368 <166840578+shambalaji368@users.noreply.github.com>
2025-04-14 12:00:10 +0530

    vehicle detail changes

--- CarParkingBooking_UI/src/app/Dashboard/User/uservehicle/user-vehicle.component.ts
+++ CarParkingBooking_UI/src/app/Dashboard/User/uservehicle/user-vehicle.component.ts
@@ -4,11 +4,22 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { VehicleDetialsService } from '../../../Service/Backend/vehicle-detials.service';
 import { CameraCaptureComponent } from '../../../shared/camera-capture/camera-capture.component';
 import { SlidersComponent } from '../../../shared/sliders/sliders.component';
+import { CommonService } from '../../service/common.service';
+import { ValueValidatorsComponent } from '../../../shared/value-validators/value-validators.component';
+import { isValid } from 'ngx-bootstrap/chronos/create/valid';
+import { ErrorMessageComponent } from '../../../shared/error-message/error-message.component';
+import { miniVehicleModal, VehicleModal } from '../../../Service/Model/VehicleModal';
 
 @Component({
   selector: 'app-uservehicle',
   standalone: true,
-  imports: [ReactiveFormsModule, CameraCaptureComponent, SlidersComponent],
+  imports: [
+    ReactiveFormsModule,
+    CameraCaptureComponent,
+    SlidersComponent,
+    ValueValidatorsComponent,
+    ErrorMessageComponent,
+  ],
   templateUrl: './user-vehicle.component.html',
   styleUrl: './user-vehicle.component.css',
 })
@@ -20,6 +31,8 @@ export class UserVehicleComponent implements AfterViewInit, OnInit {
     private router: Router,
     private activateRoute: ActivatedRoute,
     private backVehicle: VehicleDetialsService,
+    protected commonService: CommonService,
+    private vehicleDetailsService: VehicleDetialsService,
   ) {}
 
   ngOnInit(): void {
@@ -28,6 +41,7 @@ export class UserVehicleComponent implements AfterViewInit, OnInit {
       VehicleNumberImage: new FormControl('', [Validators.required]),
       VehicleImage: new FormControl('', [Validators.required]),
       OwnerName: new FormControl('', [Validators.required]),
+      DriverName: new FormControl(),
       DriverNumber: new FormControl(),
       vehicleModal: new FormControl('', [Validators.required]),
       mobileNumber: new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]),
@@ -47,11 +61,44 @@ export class UserVehicleComponent implements AfterViewInit, OnInit {
     });
   }
 
-  userconfirmbooking() {
+  confirmAddVehicle() {
+    console.log(this.vehicleDetails);
+
+    const vehicleDetail = {
+      vehicleNumber: this.vehicleDetails.get('VehicleNumber')?.value,
+      vehicleImage: this.vehicleDetails.get('VehicleImage')?.value,
+      vehicleNumberImage: this.vehicleDetails.get('VehicleNumberImage')?.value,
+      driverName: this.vehicleDetails.get('OwnerName')?.value,
+      driverPhoneNumber: this.vehicleDetails.get('DriverNumber')?.value,
+      vehicleModel: this.vehicleDetails.get('vehicleModal')?.value,
+      alternative_Phone_Number: this.vehicleDetails.get('mobileNumber')?.value,
+    } as VehicleModal;
+
+    if (!this.vehicleDetails.errors) {
+      this.vehicleDetailsService.addVehicleDetails(this.editVehicleDetails).subscribe((res) => {
+        if (res.status === 200) {
+          alert('Vehicle Details Added Successfully');
+          this.router.navigate(['/main/dealer-details']);
+        } else {
+          alert('Failed to add vehicle details');
+        }
+      });
+    } else {
+      alert('Please fill all the required fields');
+    }
+
     // this.router.navigate(['/main/dealer-details']);
   }
 
   setImage($event: string, formControlName: string) {
     this.vehicleDetails.get(formControlName)?.setValue($event);
   }
+
+  cancel() {
+    this.commonService.cancel();
+  }
+
+  isValid = (formControlName: any) => {
+    return this.vehicleDetails.get(formControlName)?.errors;
+  };
 }
---
+++ CarParkingBooking_UI/src/app/Dashboard/service/common.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommonService {
+  constructor() {}
+
+  cancel() {
+    window.history.back();
+  }
+}
